20020ec6c6d6f52f67a64663b41b552f16d78dfc
LeoSandrini <thesandrini@gmail.com>
2021-08-09 15:30:24 -0300

    Organizing project in separeted files

---
+++ src/Colors.js
@@ -0,0 +1,11 @@
+import React from 'React';
+
+// Color Theme
+const Colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue'
+};
+
+export default Colors;
