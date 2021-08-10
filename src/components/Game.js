8b6e37dd31226fcf23bc6e9011280c9afe3f13a8
LeoSandrini <thesandrini@gmail.com>
2021-08-10 18:05:59 -0300

    organizing the project in new components

---
+++ src/components/Game.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import NumberButton from './NumberButton';
+import StarsDisplay from './StarsDisplay';
+import PlayAgain from './PlayAgain';
+import Utils from '../Utils';
+import Colors from '../Colors';
+
+const Game = () => {
+  const [stars, setStars] = useState(Utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(Utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+  const [secondsLeft, setSecondsLeft] = useState(10);
+
+  const candidatesAreWrong = Utils.sum(candidateNums) > stars;
+  const gameStatus =
+    availableNums.length === 0 ? 'won' : secondsLeft === 0 ? 'lost' : 'active';
+
+  useEffect(() => {
+    if (secondsLeft > 0 && availableNums.length > 0) {
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  });
+
+  const numberStatus = number => {
+    if (!availableNums.includes(number)) {
+      return 'used';
+    }
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? 'wrong' : 'candidate';
+    }
+    return 'available';
+  };
+
+  const resetGame = () => {
+    setStars(Utils.random(1, 9));
+    setAvailableNums(Utils.range(1, 9));
+    setCandidateNums([]);
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (gameStatus !== 'active' || currentStatus === 'used') {
+      return;
+    }
+    const newCandidateNums =
+      currentStatus === 'available'
+        ? candidateNums.concat(number)
+        : candidateNums.filter(cn => cn !== number);
+    if (Utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(Utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {gameStatus !== 'active' ? (
+            <PlayAgain onClick={resetGame} gameStatus={gameStatus} />
+          ) : (
+            <StarsDisplay count={stars} />
+          )}
+        </div>
+        <div className="right">
+          {Utils.range(1, 9).map(number => (
+            <NumberButton
+              key={number}
+              status={numberStatus(number)}
+              number={number}
+              onClick={onNumberClick}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: {secondsLeft}</div>
+    </div>
+  );
+};
+
+export default Game;
